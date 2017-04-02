29507a5bb7ce2009f25b7e05f0ce295b68e45e1e
ChrKroer <chrkroer@gmail.com>
2017-04-01 22:58:36 -0400

    added pretty printing of Eigen vector

--- src/basic_types.h
+++ src/basic_types.h
@@ -1,3 +1,15 @@
+#ifndef ROBUST_CPP_BASIC_TYPES_H
+#define ROBUST_CPP_BASIC_TYPES_H
+
+#include <iostream>
 #include <Eigen/Core>
 
 using vector_d = Eigen::VectorXd;
+
+inline void pretty_print(vector_d x) {
+  Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
+                               ", ", ", ", "", "", " << ", ";");
+  std::cout << x.format(CommaInitFmt) << std::endl;
+}
+
+#endif // ROBUST_CPP_BASIC_TYPES_H
