f397f3f306d90bb0b6367213bc936ba7b90d5a96
ChrKroer <chrkroer@gmail.com>
2017-04-04 23:06:42 -0400

    added uncertainty types and made uncertainty set an interface

--- src/model/uncertainty_set.h
+++ src/model/uncertainty_set.h
@@ -5,15 +5,17 @@
 #ifndef ROBUST_CPP_UNCERTAINTY_SET_H
 #define ROBUST_CPP_UNCERTAINTY_SET_H
 
+#include "../domain/domain.h"
+
 class uncertainty_set {
 public:
-  explicit uncertainty_set(int dimension) : dimension_(dimension) {}
+  enum uncertainty_type { euclidean_ball, simplex, point };
   ~uncertainty_set() {}
-
-  int dimension() { return dimension_; }
-
-private:
-  int dimension_;
+  virtual uncertainty_type type() = 0;
+  virtual int dimension() = 0;
+  virtual vector_d center() = 0;
+  virtual std::pair<double, vector_d> maximizer(vector_d current) = 0;
+  virtual std::pair<double, vector_d> oco_update(vector_d current) = 0;
 };
 
 #endif // ROBUST_CPP_UNCERTAINTY_SET_H
