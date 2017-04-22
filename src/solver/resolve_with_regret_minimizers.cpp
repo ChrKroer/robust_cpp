220654b4d84ec767b50cde91602754a70543ae7c
ChrKroer <chrkroer@gmail.com>
2017-04-21 23:55:39 -0400

    added resolve with regret minimizers code

--- src/solver/resolve_with_regret_minimizers.cpp
+++ src/solver/resolve_with_regret_minimizers.cpp
@@ -17,28 +17,38 @@ resolve_with_regret_minimizers::resolve_with_regret_minimizers(
     rms_[constraint_id] =
         std::make_unique<online_mirror_descent>(&unc_set.get_domain());
   }
+
+  solution_ = vector_d(rp_->dimension());
+  current_ = vector_d::Zero(rp_->dimension());
+  for (int i = 0; i < rp_->dimension(); i++) {
+    current_(i) = grb_model_->getVar(i).get(GRB_DoubleAttr_X);
+  }
 }
 
 double resolve_with_regret_minimizers::optimize(int iterations_to_perform) {
   double max_gap = 0;
   double objective;
-  int iterations = 0;
-  while (iterations < iterations_to_perform) {
-    vector_d current_solution = current_strategy();
+  int prev_iterations = iterations_;
+  while (iterations_ < prev_iterations + iterations_to_perform) {
     for (auto it = rp_->robust_constraints_begin();
          it != rp_->robust_constraints_end(); ++it) {
       int constraint_id = *it;
       const uncertainty_constraint &unc_set =
           rp_->get_uncertainty_constraint(constraint_id);
-      vector_d g = unc_set.gradient(current_solution);
+      vector_d g = unc_set.gradient(current_);
       rms_[constraint_id]->receive_gradient(g);
       vector_d unc_set_current = rms_[constraint_id]->get_current_solution();
       update_uncertainty_constraint(constraint_id, unc_set_current);
     }
     grb_model_->optimize();
-    iterations++;
+    int model_status = grb_model_->get(GRB_IntAttr_Status);
+    if (model_status != GRB_OPTIMAL) {
+      logger->info("Model not optimal, status: {}", model_status);
+    }
+    iterations_++;
+    update_solution();
     objective = grb_model_->get(GRB_DoubleAttr_ObjVal);
-    logger->info("objective on iteration {}: {}", iterations, objective);
+    logger->info("objective on iteration {}: {}", iterations_, objective);
   }
 
   return objective;
@@ -46,14 +56,17 @@ double resolve_with_regret_minimizers::optimize(int iterations_to_perform) {
 
 void resolve_with_regret_minimizers::update_uncertainty_constraint(
     int constraint_id, const vector_d &coeff) {
-  // TODO: implement this
   assert(coeff.size() == rp_->dimension());
+  GRBConstr constr = grb_model_->getConstr(constraint_id);
+  for (int i = 0; i < coeff.size(); i++) {
+    GRBVar v = grb_model_->getVar(i);
+    grb_model_->chgCoeff(constr, v, coeff(i));
+  }
 }
 
-vector_d resolve_with_regret_minimizers::current_strategy() {
-  vector_d strategy(rp_->dimension());
+void resolve_with_regret_minimizers::update_solution() {
   for (int i = 0; i < rp_->dimension(); i++) {
-    strategy(i) = grb_model_->getVar(i).get(GRB_DoubleAttr_X);
+    current_(i) = grb_model_->getVar(i).get(GRB_DoubleAttr_X);
+    solution_(i) += current_(i);
   }
-  return strategy;
 }
--- src/solver/resolve_with_regret_minimizers.h
+++ src/solver/resolve_with_regret_minimizers.h
@@ -16,18 +16,23 @@ public:
   explicit resolve_with_regret_minimizers(const robust_program_dense *rp);
   ~resolve_with_regret_minimizers() {}
 
-  vector_d current_strategy();
+  vector_d current_solution() { return solution_ / iterations_; }
   double optimize(int iterations_to_perform);
 
 private:
-  void update_uncertainty_constraint(int constraint_id, const vector_d &coeff);
   const robust_program *rp_;
   // indexed by the constraint_id that the rms belongs to
   std::unordered_map<int, std::unique_ptr<online_gradient_method>> rms_;
 
   GRBEnv grb_env_;
   std::unique_ptr<GRBModel> grb_model_;
-  double tolerance = 1e-6;
+  double tolerance_ = 1e-6;
+  int iterations_ = 0;
+  vector_d solution_;
+  vector_d current_;
+
+  void update_uncertainty_constraint(int constraint_id, const vector_d &coeff);
+  void update_solution();
 };
 
 #endif // ROBUST_CPP_RESOLVE_WITH_REGRET_MINIMIZERS_H
