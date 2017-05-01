37bccfc77cae1fd9a09936123e5433b32c0fd924
ChrKroer <chrkroer@gmail.com>
2017-04-30 20:34:06 -0400

    updated uncertainty_constraint interface to not have center and radius, removed constraint type, and added violation amount function

--- src/model/uncertainty_constraint.h
+++ src/model/uncertainty_constraint.h
@@ -5,25 +5,25 @@
 #ifndef ROBUST_CPP_UNCERTAINTY_CONSTRAINT_H
 #define ROBUST_CPP_UNCERTAINTY_CONSTRAINT_H
 
-#include "./../domain/domain.h"
 #include "./../basic_types.h"
+#include "./../domain/domain.h"
 
 class uncertainty_constraint {
 public:
-  enum constraint_type { EUCLIDEAN_BALL, SIMPLEX, POINT };
+  // enum constraint_type { EUCLIDEAN_BALL, SIMPLEX, POINT };
   enum function_type { LINEAR };
   ~uncertainty_constraint() {}
-  virtual constraint_type get_set_type() const = 0;
   virtual function_type get_function_type() const = 0;
   virtual int dimension() const = 0;
-  virtual vector_d center() const = 0;
-  virtual double radius() const = 0;
   // returns the parameter instantiation that maximizes the constraint function,
   // as well as the constraint value.
-  virtual std::pair<double, vector_d> maximizer(const vector_d current) const = 0;
+  virtual std::pair<double, vector_d>
+  maximizer(const vector_d current) const = 0;
   virtual vector_d gradient(const vector_d &current) const = 0;
   virtual const std::vector<int> &uncertainty_variable_ids() const = 0;
   virtual const domain &get_domain() const = 0;
+  virtual double violation_amount(const vector_d &solution,
+                                  const vector_d &constraint_params) const = 0;
 };
 
 #endif // ROBUST_CPP_UNCERTAINTY_CONSTRAINT_H
