b1e7d2e70060324098e07f17f888b93a1506dfee
ChrKroer <chrkroer@gmail.com>
2017-05-03 19:01:07 -0400

    added argument parser to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include "./../external_code/cxxopts.hpp"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
+  cxxopts::Options options("robust_cpp", "Solve robust optimization problems");
 
+  // turn off clang-format as it does not handle the currying well
+  // clang-format off
+  options.add_options()
+    ("m,model", "Nominal model mps file", cxxopts::value<std::string>())
+    ("r,robust", "Robustness file", cxxopts::value<std::string>())
+    ("a,algorithm", "Algorithms: pessimization, oco_opt, oco (TODO)",
+     cxxopts::value<std::string>())
+    ;
+  // clang-format on
 }
