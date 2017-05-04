439cd20daae9550ec6d185942ca81443d37f7c62
ChrKroer <chrkroer@gmail.com>
2017-05-03 21:33:06 -0400

    parsing of args

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include "./../external_code/cxxopts.hpp"
+#include "./logging.h"
+#include "./solver/pessimization_solver.h"
+#include "./solver/resolve_with_regret_minimizers.h"
 
 int main(int argc, char *argv[]) {
   cxxopts::Options options("robust_cpp", "Solve robust optimization problems");
@@ -12,4 +15,8 @@ int main(int argc, char *argv[]) {
      cxxopts::value<std::string>())
     ;
   // clang-format on
+  options.parse(argc, argv);
+  string nominal_file = options["model"].as<string>();
+  string robust_file = options["robust"].as<string>();
+  string algorithm = options["algorithm"].as<string>();
 }
