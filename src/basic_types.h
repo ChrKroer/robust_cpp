2cf7c74da2993c49ebb9fda4ce6e053997717267
ChrKroer <chrkroer@gmail.com>
2017-05-05 22:11:06 -0400

    added functions to generate string representations of eigen vec and sparse coeff vec

--- src/basic_types.h
+++ src/basic_types.h
@@ -3,6 +3,7 @@
 
 #include "Eigen/Core"
 #include <iostream>
+#include <vector>
 
 #define TRACE_MSG                                                              \
   fprintf(stderr, "[%s:%d] %s here I am\n", __FILE__, __LINE__,                 \
@@ -14,6 +15,22 @@ inline void pretty_print(vector_d x) {
   Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
                                ", ", ", ", "", "", " << ", ";");
   std::cout << x.format(CommaInitFmt) << std::endl;
+}
+
+inline std::string eigen_to_string(vector_d x) {
+  Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
+                               ", ", ", ", "", "", " << ", ";");
+  std::stringstream ss;
+  ss << x.format(CommaInitFmt);
+  return ss.str();
+}
+
+inline std::string sparse_vector_string(std::vector<std::pair<int,double>> &x) {
+  std::stringstream ss;
+  for (auto p : x) {
+    ss << "<" << p.first << "," << p.second << "> ";
+  }
+  return ss.str();
 }
 
 #endif // ROBUST_CPP_BASIC_TYPES_H
