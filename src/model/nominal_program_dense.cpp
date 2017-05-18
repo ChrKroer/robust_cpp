fb9aaa8386be02e0f63d9d3e2d8e127b81506f18
ChrKroer <chrkroer@gmail.com>
2017-05-17 22:10:19 -0400

    using gurobi to parse model in nominal program dense

--- src/model/nominal_program_dense.cpp
+++ src/model/nominal_program_dense.cpp
@@ -4,17 +4,19 @@
 
 #include "./nominal_program_dense.h"
 #include "./../logging.h"
+#include "gurobi_c++.h"
 #include <boost/filesystem.hpp>
 
 nominal_program_dense::nominal_program_dense(std::string model_path)
     : model_path_(model_path) {
 
   if (boost::filesystem::extension(model_path).compare(".mps") == 0) {
-    clp_model_.setLogLevel(0);
-    clp_model_.readMps(model_path.c_str());
+    GRBEnv env;
+    env.set(GRB_IntParam_OutputFlag, 0);
+    GRBModel model(env, model_path);
+    dimension_ = model.get(GRB_IntAttr_NumVars);
+    num_constraints_ = model.get(GRB_IntAttr_NumConstrs);
   } else {
     logger->error("unsupported nominal program file type");
   }
-  dimension_ = clp_model_.getNumCols();
-  num_constraints_ = clp_model_.getNumRows();
 }
--- src/model/nominal_program_dense.h
+++ src/model/nominal_program_dense.h
@@ -5,11 +5,8 @@
 #ifndef ROBUST_CPP_NOMINAL_PROGRAM_DENSE_H
 #define ROBUST_CPP_NOMINAL_PROGRAM_DENSE_H
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-register"
-#include "../../external_code/Clp-1.15.6/include/coin/ClpModel.hpp"
-#pragma clang diagnostic pop
 #include "./nominal_program.h"
+#include <string>
 
 class nominal_program_dense : public nominal_program {
 public:
@@ -23,7 +20,6 @@ private:
   int dimension_;
   int num_constraints_;
   std::string model_path_;
-  ClpModel clp_model_;
 };
 
 #endif // ROBUST_CPP_NOMINAL_PROGRAM_DENSE_H
