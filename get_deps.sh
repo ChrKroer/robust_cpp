323a561462a9ecf3aed91dbd9f10c7917af2a856
ChrKroer <chrkroer@gmail.com>
2017-07-02 11:24:20 -0400

    added script for downloading dependencies

---
+++ get_deps.sh
@@ -0,0 +1,5 @@
+
+mkdir external_code
+cd external_code
+wget https://raw.githubusercontent.com/nlohmann/json/develop/src/json.hpp
+wget https://raw.githubusercontent.com/jarro2783/cxxopts/master/include/cxxopts.hpp
