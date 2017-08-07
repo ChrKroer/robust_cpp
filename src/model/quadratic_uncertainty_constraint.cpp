98146727493fc286f5864c1643324c5d03e726f8
ChrKroer <chrkroer@gmail.com>
2017-08-06 22:54:46 -0400

    fixed violation_amount computation

--- src/model/quadratic_uncertainty_constraint.cpp
+++ src/model/quadratic_uncertainty_constraint.cpp
@@ -2,34 +2,35 @@
 // Created by Christian Kroer on 5/01/17.
 //
 
-#include "./quadratic_uncertainty_constraint.h"
 #include "./../logging.h"
 #include "./../solver/trust_region.h"
+#include "./quadratic_uncertainty_constraint.h"
 #include "Eigen/Eigenvalues"
-
 
 quadratic_uncertainty_constraint::quadratic_uncertainty_constraint(
     int constraint_id, std::unique_ptr<domain> dom, matrix_d base_matrix,
-    std::vector<int> nominal_indices, std::vector<matrix_d> uncertain_matrices
-    , std::vector<double> certain_variable_coefficient, std::vector<std::string> certain_variable_name,
-    double rhs)
-    : constraint_id_(constraint_id), domain_(std::move(dom)),
-      base_matrix_(base_matrix), nominal_indices_(nominal_indices),
-      uncertain_matrices_(uncertain_matrices), 
+    std::vector<int> nominal_indices, std::vector<matrix_d> uncertain_matrices,
+    std::vector<double> certain_variable_coefficient,
+    std::vector<std::string> certain_variable_name, double rhs)
+    : constraint_id_(constraint_id),
+      domain_(std::move(dom)),
+      base_matrix_(base_matrix),
+      nominal_indices_(nominal_indices),
+      uncertain_matrices_(uncertain_matrices),
       rhs_(rhs) {
-        certain_variable_name_ = certain_variable_name;
-        certain_variable_coefficient_ = certain_variable_coefficient;
-      }
+  certain_variable_name_ = certain_variable_name;
+  certain_variable_coefficient_ = certain_variable_coefficient;
+}
 
-std::pair<double, vector_d>
-quadratic_uncertainty_constraint::maximizer(const vector_d current) const {
+std::pair<double, vector_d> quadratic_uncertainty_constraint::maximizer(
+    const vector_d current) const {
   std::pair<double, vector_d> trs = trs_subproblem_solution(current);
   double violation = violation_amount(current, trs.second);
   return std::make_pair(violation, trs.second);
 }
 
-vector_d
-quadratic_uncertainty_constraint::gradient(const vector_d &current) const {
+vector_d quadratic_uncertainty_constraint::gradient(
+    const vector_d &current) const {
   vector_d g = vector_d(domain_->dimension());
   // TODO: write this
   return g;
@@ -38,18 +39,12 @@ quadratic_uncertainty_constraint::gradient(const vector_d &current) const {
 double quadratic_uncertainty_constraint::violation_amount(
     const vector_d &solution, const vector_d &constraint_params) const {
   vector_d nominal_subset = get_nominal_active_variables(solution);
-  double val = 0;
-  val += (base_matrix_ * nominal_subset).squaredNorm();
-  for (int i = 0; i < constraint_params.size(); i++) {
-    const matrix_d &m = uncertain_matrices_[i];
-    val += (m * nominal_subset).dot(base_matrix_ * nominal_subset) *
-           constraint_params(i);
-  }
-
-  return val - rhs_;
+  matrix_d m = get_matrix_instantiation(constraint_params);
+  // TODO(chrkroer): add certain coefficients here
+  return (m * nominal_subset).squaredNorm() - rhs_;
 }
 
-//removes inactive entries nominal solution
+// removes inactive entries nominal solution
 vector_d quadratic_uncertainty_constraint::get_nominal_active_variables(
     const vector_d nominal_solution) const {
   vector_d v(nominal_indices_.size());
@@ -68,7 +63,7 @@ matrix_d quadratic_uncertainty_constraint::get_matrix_instantiation(
   return m;
 }
 
-//Y(x) := \{x^\top P_j^\top P_{j'} x\}_{j,j' \in [k]} \in R^{k x k}
+// Y(x) := \{x^\top P_j^\top P_{j'} x\}_{j,j' \in [k]} \in R^{k x k}
 matrix_d quadratic_uncertainty_constraint::get_pairwise_uncertainty_quadratic(
     const vector_d &nominal_solution) const {
   const vector_d nominal_subset =
@@ -84,7 +79,7 @@ matrix_d quadratic_uncertainty_constraint::get_pairwise_uncertainty_quadratic(
   return Y;
 }
 
-//b(x) := \{x^\top A_0^\top P_{j} x\}_{j \in [k]} \in R^k
+// b(x) := \{x^\top A_0^\top P_{j} x\}_{j \in [k]} \in R^k
 vector_d quadratic_uncertainty_constraint::get_linear_uncertainty_coefficients(
     const vector_d &nominal_solution) const {
   const vector_d nominal_subset =
